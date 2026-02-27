12e516def7f4d9a1a97aae99f9fb8b31768ca327
Wyatt <wyatt102903@gmail.com>
2026-02-27 12:59:04 -0500

    2/27/26 1pm
    Added in autoclicker. but havent set them up just added them to the UI and stuff. and added star bonus.

--- StarSpawner.gd
+++ StarSpawner.gd
@@ -7,14 +7,30 @@ extends Node2D
 
 # optional: speed range per star
 @export var min_speed: float = 50.0
-@export var max_speed: float = 12.0
+@export var max_speed: float = 120.0
 
 @onready var timer: Timer = $Timer
+var spawn_paused: bool = false
 
 func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
+	
+func pause_spawning_for(seconds: float) -> void:
+	if spawn_paused:
+		return
+	spawn_paused = true
+	
+	# stop the spawn timer so nothing else can spawn
+	timer.stop()
+	await get_tree().create_timer(seconds).timeout
+	spawn_paused = false
+	timer.start()
 
 func _on_timer_timeout() -> void:
+	
+	if spawn_paused:
+		return
+		
 	if randf() > spawn_chance:
 		return
 
@@ -28,5 +44,9 @@ func _on_timer_timeout() -> void:
 	# random fall speed
 	star.fall_speed = randf_range(min_speed, max_speed)
 
+
 	# when clicked, tell the main game to apply a random effect
-	star.clicked.connect(func(): get_tree().call_group("game", "apply_random_star_effect"))
+	star.clicked.connect(func(): 
+		#pause spawns for 10 seconds when clicked
+		pause_spawning_for(10.0)
+		get_tree().call_group("game", "apply_random_star_effect"))
